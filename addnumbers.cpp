420c056a06bf6aa8dcee85ec02bd8cea728d6884
devtejas01 <ktechnicalsitsme@gmail.com>
2025-01-07 04:05:41 +0530

    Initial commit

---
+++ addnumbers.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int num,i=0,sum=0,kn;
+ cin>>num;
+ int arr[4];
+ while (num!=0)
+ {
+    k=num-(num/10)*10;
+    arr[i]=k;
+    cout<<arr[i];
+    num=num/10;
+    
+    
+
+ }
+ while (sum<10)
+ {
+    sum=sum+arr[i];
+    if (sum>10)
+    { newsum=sum; 
+        while ()
+        {
+            /* code */
+        }
+        
+    }
+    
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

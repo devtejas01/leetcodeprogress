7aa70a5f14b84dff73fd88a027554082bdce146e
devtejas01 <ktechnicalsitsme@gmail.com>
2025-01-08 01:04:02 +0530

    adding new leetcode question 1

--- addnumbers.cpp
+++ addnumbers.cpp
@@ -1,50 +1,39 @@
 #include<iostream>
 using namespace std;
 int main()
-{
- int num,i=0,sum=0,kn;
- cin>>num;
- int arr[4];
- while (num!=0)
- {
-    k=num-(num/10)*10;
-    arr[i]=k;
-    cout<<arr[i];
-    num=num/10;
-    
-    
+{ int i=0,j=0,newsum=0,sum=0,n,k=0;
+cin>>n;
+int arr[16];
+while (n!=0)
+{ i=n/10;
+  arr[i]=n-i*10;
+  sum=sum+arr[i];
+  n=n/10;
+  i++;
 
- }
- while (sum<10)
- {
-    sum=sum+arr[i];
-    if (sum>10)
-    { newsum=sum; 
-        while ()
-        {
-            /* code */
-        }
+    
+}
+k=sum,i=0;
+while (sum>10)
+{
+    if (k==0)
+    {   sum=newsum;
         
     }
+   
+   else{
+    j=k/10;
+    arr[i]=k-j*10;
+    newsum=newsum+arr[i];
+    k=k/10;
+    i++;
+   }
     
- }
- 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+cout<<sum;
 
 
 
